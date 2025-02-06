433f152757c634c48867ffd0589b16af70514749
Jonathan EP <jonathan.perez@ep.net.pe>
2025-02-06 10:16:43 -0500

    first commit

---
+++ src/main/java/com/electronicpayment/televentas/EpTeleventasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.electronicpayment.televentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpTeleventasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpTeleventasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electronicpayment/televentas/EpTeleventasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronicpayment.televentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpTeleventasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
