217e5f040644b711e39e9b39c1136253989d1cfa
Jonathan EP <jonathan.perez@ep.net.pe>
2025-02-06 10:56:18 -0500

    preparación de properties

---
+++ src/main/java/com/electronicpayment/televentas/config/DatasourceConfiguration.java
@@ -0,0 +1,24 @@
+package com.electronicpayment.televentas.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DatasourceConfiguration {
+    @Bean(name = "dbTeleventasDataSource")
+    @ConfigurationProperties(prefix ="spring.datasource.televentas")
+    public DataSource dbTeleventasHikariConfig() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JdbcTemplate dbTeleventasJdbcTemplate(@Qualifier("dbTeleventasDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
